116d0902e64c565c958d25a1239c4a5c82e522ba
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2025-04-28 15:06:40 +0200

    use autoFlag

--- flagset/frontend.go
+++ flagset/frontend.go
@@ -17,15 +17,3 @@ func Build(c *puzzle.Config, name string, h flag.ErrorHandling) (*flag.FlagSet, 
 	}
 	return flagset, nil
 }
-
-// func ToFlags(c *puzzle.Config) []string {
-// 	out := make([]string, 0)
-// 	for e := range c.Entries() {
-// 		fn := e.GetMetadata().FlagName
-// 		if fn == "" {
-// 			continue
-// 		}
-// 		out = append(out, fmt.Sprintf("-%s=%s", fn, e.String()))
-// 	}
-// 	return out
-// }
--- flagset/types.go
+++ flagset/types.go
@@ -8,131 +8,30 @@ import (
 	"github.com/asiffer/puzzle"
 )
 
-// during the init phase we inject the frontend into every converter
 func init() {
-	// default
-	puzzle.BoolConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(boolFlag)))
-	puzzle.DurationConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(durationFlag)))
-	puzzle.Float64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(float64Flag)))
-	puzzle.IntConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(intFlag)))
-	puzzle.Int64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(int64Flag)))
-	puzzle.StringConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(stringFlag)))
-	puzzle.UintConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(uintFlag)))
-	puzzle.Uint64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(uint64Flag)))
-	// extended
-	puzzle.BytesConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(bytesFlag)))
-	puzzle.IPConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(ipFlag)))
-	puzzle.Int8Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(int8Flag)))
-	puzzle.Int16Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(int16Flag)))
-	puzzle.Int32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(int32Flag)))
-	puzzle.Uint8Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(uint8Flag)))
-	puzzle.Uint16Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(uint16Flag)))
-	puzzle.Uint32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(uint32Flag)))
-	puzzle.StringSliceConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(stringSliceFlag)))
-	puzzle.Float32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(ignoreFlag(float32Flag)))
+	puzzle.BoolConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[bool]))
+	puzzle.DurationConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[time.Duration]))
+	puzzle.Float64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[float64]))
+	puzzle.IntConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[int]))
+	puzzle.Int64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[int64]))
+	puzzle.StringConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[string]))
+	puzzle.UintConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[uint]))
+	puzzle.Uint64Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[uint64]))
+	puzzle.BytesConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[[]byte]))
+	puzzle.IPConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[net.IP]))
+	puzzle.Int8Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[int8]))
+	puzzle.Int16Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[int16]))
+	puzzle.Int32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[int32]))
+	puzzle.Uint8Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[uint8]))
+	puzzle.Uint16Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[uint16]))
+	puzzle.Uint32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[uint32]))
+	puzzle.StringSliceConverter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[[]string]))
+	puzzle.Float32Converter.Register(FlagFrontend, puzzle.ConvertCallbackFactory1(autoFlag[float32]))
 }
 
-// supported types
-
-type flagFun[T any] func(entry *puzzle.Entry[T], fs *flag.FlagSet) error
-
-func ignoreFlag[T any](f flagFun[T]) flagFun[T] {
-	return func(entry *puzzle.Entry[T], fs *flag.FlagSet) error {
-		if entry.Metadata.FlagName == "" {
-			return nil
-		}
-		return f(entry, fs)
+func autoFlag[T any](entry *puzzle.Entry[T], fs *flag.FlagSet) error {
+	if entry.Metadata.FlagName != "" {
+		fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
 	}
-}
-
-func boolFlag(entry *puzzle.Entry[bool], fs *flag.FlagSet) error {
-	fs.BoolVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func durationFlag(entry *puzzle.Entry[time.Duration], fs *flag.FlagSet) error {
-	fs.DurationVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func float64Flag(entry *puzzle.Entry[float64], fs *flag.FlagSet) error {
-	fs.Float64Var(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func intFlag(entry *puzzle.Entry[int], fs *flag.FlagSet) error {
-	fs.IntVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func int64Flag(entry *puzzle.Entry[int64], fs *flag.FlagSet) error {
-	fs.Int64Var(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func stringFlag(entry *puzzle.Entry[string], fs *flag.FlagSet) error {
-	fs.StringVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func uintFlag(entry *puzzle.Entry[uint], fs *flag.FlagSet) error {
-	fs.UintVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func uint64Flag(entry *puzzle.Entry[uint64], fs *flag.FlagSet) error {
-	fs.Uint64Var(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-// extended
-
-func bytesFlag(entry *puzzle.Entry[[]byte], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func ipFlag(entry *puzzle.Entry[net.IP], fs *flag.FlagSet) error {
-	fs.TextVar(entry.ValueP, entry.Metadata.FlagName, entry.Value, entry.Metadata.Description)
-	return nil
-}
-
-func int8Flag(entry *puzzle.Entry[int8], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func int16Flag(entry *puzzle.Entry[int16], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func int32Flag(entry *puzzle.Entry[int32], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func uint8Flag(entry *puzzle.Entry[uint8], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func uint16Flag(entry *puzzle.Entry[uint16], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func uint32Flag(entry *puzzle.Entry[uint32], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func stringSliceFlag(entry *puzzle.Entry[[]string], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
-	return nil
-}
-
-func float32Flag(entry *puzzle.Entry[float32], fs *flag.FlagSet) error {
-	fs.Var(entry, entry.Metadata.FlagName, entry.Metadata.Description)
 	return nil
 }
