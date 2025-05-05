fbe9a07729bc94a780e8ab2d55d8df70b32083ca
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2025-05-05 10:13:46 +0200

    populate flagset

--- flagset/frontend.go
+++ flagset/frontend.go
@@ -10,10 +10,14 @@ const FlagFrontend puzzle.Frontend = "flagset"
 
 func Build(c *puzzle.Config, name string, h flag.ErrorHandling) (*flag.FlagSet, error) {
 	flagset := flag.NewFlagSet(name, h)
+	return flagset, Populate(c, flagset)
+}
+
+func Populate(c *puzzle.Config, flagset *flag.FlagSet) error {
 	for entry := range c.Entries() {
 		if err := entry.Convert(FlagFrontend, flagset); err != nil {
-			return nil, err
+			return err
 		}
 	}
-	return flagset, nil
+	return nil
 }
